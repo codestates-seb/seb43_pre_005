f578434fc66eb14d3b8295314162105c40e352f7
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-04-17 21:17:16 +0900

    [Feat][FE] FE공통컴포넌트 수정
    - Route처리
    - Layout으로 묶어줌

---
+++ client/src/pages/Home.js
@@ -0,0 +1,7 @@
+import Layout from "../components/common/Layout";
+
+const Home = () => {
+  return <Layout>hello home</Layout>;
+};
+
+export default Home;
---
+++ client/src/pages/Tags.js
@@ -0,0 +1,7 @@
+import Layout from "../components/common/Layout";
+
+const Tags = () => {
+  return <Layout>hi tag</Layout>;
+};
+
+export default Tags;
