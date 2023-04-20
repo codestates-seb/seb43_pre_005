4595c4fe0b182927e9e947229a270a2de783d178
digital-hamster <waterlove1439@naver.com>
2023-04-20 19:04:12 +0900

    - Users Page 완성 - 한 페이지에 들어갈 컴포넌트의 수는 10개로 임시 지정

--- src/main/java/com/potatos/stackoverflow/domain/member/controller/MemberController.java
+++ src/main/java/com/potatos/stackoverflow/domain/member/controller/MemberController.java
@@ -1,14 +1,17 @@
 package com.potatos.stackoverflow.domain.member.controller;
 
+import com.potatos.stackoverflow.domain.member.dto.MembersPageDto;
 import com.potatos.stackoverflow.domain.member.service.MemberService;
 import com.potatos.stackoverflow.domain.member.dto.MemberPostDto;
 import com.potatos.stackoverflow.domain.member.dto.MemberResponseDto;
 import com.potatos.stackoverflow.util.UriCreator;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.net.URI;
+import java.util.List;
 
 @RequestMapping("/users")
 @RestController
@@ -29,5 +32,14 @@ public class MemberController {
         URI location = UriCreator.createUri("/users", 1);
 
         return ResponseEntity.created(location).build();
+    }
+
+
+    @GetMapping("/group")
+    public ResponseEntity getUsers(@RequestParam(value = "page", defaultValue = "0") int page) {
+
+        List<MembersPageDto> response = this.memberService.getMembersPage(page);
+
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
