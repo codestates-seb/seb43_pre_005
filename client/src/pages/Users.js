297950127cc3a39ca08711f31eb94d920d819a82
ggggggggithub <hamtoli5@gmail.com>
2023-04-20 14:44:33 +0900

    [FEAT][FE] Userpage

---
+++ client/src/pages/Users.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Layout from "../components/common/Layout";
+import SearchBox from "./searchbox.jsx";
+import { dummydata } from "./userdummydata.js";
+import axios from "axios";
+
+const UserDesign = styled.div`
+  padding: 24px;
+  .usersearch {
+    padding: 2px 2px 2px 2px;
+    margin: 0 0 4px 6px;
+    text-align: left;
+    font-size: 20px;
+    line-height: 20px;
+  }
+`;
+
+const Users = () => {
+  /*   const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://localhost:3000',
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []); */
+
+  return (
+    <Layout>
+      <UserDesign>
+        <h1>Users</h1>
+        <SearchBox data={dummydata} />
+      </UserDesign>
+    </Layout>
+  );
+};
+
+export default Users;
