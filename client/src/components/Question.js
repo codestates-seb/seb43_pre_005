b3a9b1fcaae971860d50885b6fa0abe58150dcc5
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2023-04-21 15:00:45 +0900

    tag 추가

--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -12,6 +12,27 @@ const QuestionContainer = styled.div`
     display: flex;
     margin-bottom: 1rem;
   }
+
+  .name-box {
+    display: flex;
+    align-items: right;
+    justify-content: space-between;
+    margin-top: 1rem;
+
+    .question-tag {
+      border: none;
+      padding: 0.3rem;
+      border-radius: 5px;
+      background-color: #baaad7;
+      color: white;
+      cursor: pointer;
+    }
+
+    .question-name {
+      margin-left: 30rem;
+      font-weight: bold;
+    }
+  }
 `;
 
 const Question = ({ question }) => {
@@ -22,6 +43,11 @@ const Question = ({ question }) => {
       <div className="question-id">
         <div className="question-title">{question.title}</div>
         <div className="question-content">{question.content}</div>
+        <div className="name-box">
+          <div className="question-tag">{question.tag}</div>
+          <div className="question-name">{question.name}</div>
+          <div className="question-createAt">{question.createdAt}</div>
+        </div>
       </div>
     </QuestionContainer>
   );
--- client/src/data/qsdummyData.js
+++ client/src/data/qsdummyData.js
@@ -5,12 +5,18 @@ const qsdummyData = [
       "Overcoming class imbalance when fine-tuning on few data for object detection?",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 12,
     title: "Count the most common ip in all the pcap files in a folder",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 13,
@@ -18,36 +24,54 @@ const qsdummyData = [
       "Reuse the same thread function for several independent data structures",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 14,
     title: "Rust `heapless::IndexMap` implementation with custom hasher",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 15,
     title: "Rust `heapless::IndexMap` implementation with custom hasher",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 16,
     title: "Rust `heapless::IndexMap` implementation with custom hasher",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 17,
     title: "Rust `heapless::IndexMap` implementation with custom hasher",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 18,
     title: "Count the most common ip in all the pcap files in a folder",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 19,
@@ -55,12 +79,18 @@ const qsdummyData = [
       "Reuse the same thread function for several independent data structures",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
   {
     id: 20,
     title: "Rust `heapless::IndexMap` implementation with custom hasher",
     content:
       "Java is a high-level object-oriented programming language. Use this tag when you're having problems using or understanding the language itself. This tag is frequently used alongside other tags for libraries and/or frameworks used by Java developers.",
+    name: "Mango",
+    createdAt: "2023.4.21",
+    tag: "fruit",
   },
 ];
 
