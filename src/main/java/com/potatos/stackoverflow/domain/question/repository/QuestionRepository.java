c6c4367abd44844350f96e1273c1ebb851fe9ec0
digital-hamster <waterlove1439@naver.com>
2023-04-23 23:27:07 +0900

    - 검색 기능 완성 - (제목만) 특정 단어를 포함하는 검색 기능 구현 - 10개로 끊어서 전달

--- src/main/java/com/potatos/stackoverflow/domain/question/repository/QuestionRepository.java
+++ src/main/java/com/potatos/stackoverflow/domain/question/repository/QuestionRepository.java
@@ -1,7 +1,15 @@
 package com.potatos.stackoverflow.domain.question.repository;
 
 import com.potatos.stackoverflow.domain.question.entity.Question;
+import com.potatos.stackoverflow.domain.tags.entity.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository  extends JpaRepository<Question, Long> {
+
+    //search
+    Page<Question> findByTitleContaining(Pageable pageable, String word);
 }
